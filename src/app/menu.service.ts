cac7b8db98a8067c7c4ab38408694cde9456ab90
Godwill10 <88179621+Godwill10@users.noreply.github.com>
2026-08-20 22:45:48 -0500

    Add menu API service

---
+++ src/app/menu.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class MenuService {
+  private apiUrl = 'http://localhost:3000/api/menu';
+  constructor(private http: HttpClient) {}
+  getMenu(): Observable<any[]> { return this.http.get<any[]>(this.apiUrl); }
+  getItem(id: number): Observable<any> { return this.http.get<any>(`${this.apiUrl}/${id}`); }
+}

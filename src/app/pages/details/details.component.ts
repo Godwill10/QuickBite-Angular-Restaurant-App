6c9699d7df31ea328ef640a10d0c8ae00395e8b1
Godwill10 <88179621+Godwill10@users.noreply.github.com>
2026-08-20 22:49:36 -0500

    Add menu item details logic

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MenuService } from '../../menu.service';
+import { CartService } from '../../cart.service';
+
+@Component({ selector:'app-details', templateUrl:'./details.component.html', styleUrls:['./details.component.css'] })
+export class DetailsComponent implements OnInit {
+  item:any;
+  constructor(private route:ActivatedRoute, private menuService:MenuService, private cartService:CartService) {}
+  ngOnInit():void { const id=Number(this.route.snapshot.paramMap.get('id')); this.menuService.getItem(id).subscribe(data=>this.item=data); }
+  addToCart():void { if(this.item){ this.cartService.add(this.item); alert(`${this.item.name} added to cart!`); } }
+}

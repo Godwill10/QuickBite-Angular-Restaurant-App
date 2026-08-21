563aceb523dfdede5d440e4626ec7e9d8d80c7da
Godwill10 <88179621+Godwill10@users.noreply.github.com>
2026-08-20 22:46:08 -0500

    Add cart component logic

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CartService } from '../../cart.service';
+
+@Component({ selector: 'app-cart', templateUrl: './cart.component.html', styleUrls: ['./cart.component.css'] })
+export class CartComponent {
+  constructor(public cartService: CartService) {}
+  remove(id: number): void { this.cartService.remove(id); }
+  getTotal(): number { return this.cartService.getTotal(); }
+}
